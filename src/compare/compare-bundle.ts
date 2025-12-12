9f809a045293826fb7c58e9f8d7eefd59cac0b80
David Sveningsson <ext@sidvind.com>
2025-12-12 20:47:56 +0100

    refactor: split `compareBundle()` into separate functions

--- src/compare/compare-bundle.ts
+++ src/compare/compare-bundle.ts
@@ -1,6 +1,72 @@
 import type { BundleDiff } from "../bundle-diff.ts";
 import type { BundleSize } from "../bundle-size.ts";
 
+function compareUpdated(base: BundleSize, current: BundleSize): BundleDiff {
+	return {
+		status: "updated",
+		id: current.id,
+		name: current.bundle,
+		oldSize: base.size,
+		newSize: current.size,
+		sizeDiff: current.size - base.size,
+
+		oldGzip: base.gzip,
+		newGzip: current.gzip,
+		gzipDiff: current.gzip - base.gzip,
+
+		oldBrotli: base.brotli,
+		newBrotli: current.brotli,
+		brotliDiff: current.brotli - base.brotli,
+
+		oldFiles: base.files,
+		newFiles: current.files,
+	};
+}
+
+function compareAdded(current: BundleSize): BundleDiff {
+	return {
+		status: "added",
+		id: current.id,
+		name: current.bundle,
+		oldSize: 0,
+		newSize: current.size,
+		sizeDiff: current.size,
+
+		oldGzip: 0,
+		newGzip: current.gzip,
+		gzipDiff: current.gzip,
+
+		oldBrotli: 0,
+		newBrotli: current.brotli,
+		brotliDiff: current.brotli,
+
+		oldFiles: [],
+		newFiles: current.files,
+	};
+}
+
+function compareRemoved(base: BundleSize): BundleDiff {
+	return {
+		status: "removed",
+		id: base.id,
+		name: base.bundle,
+		oldSize: base.size,
+		newSize: 0,
+		sizeDiff: -base.size,
+
+		oldGzip: base.gzip,
+		newGzip: 0,
+		gzipDiff: -base.gzip,
+
+		oldBrotli: base.brotli,
+		newBrotli: 0,
+		brotliDiff: -base.brotli,
+
+		oldFiles: base.files,
+		newFiles: [],
+	};
+}
+
 /**
  * Compare two bundles and return the size difference.
  *
@@ -21,64 +87,10 @@ export function compareBundle(
 	const [base, current] = args;
 
 	if (base && current) {
-		return {
-			status: "updated",
-			id: current.id,
-			name: current.bundle,
-			oldSize: base.size,
-			newSize: current.size,
-			sizeDiff: current.size - base.size,
-
-			oldGzip: base.gzip,
-			newGzip: current.gzip,
-			gzipDiff: current.gzip - base.gzip,
-
-			oldBrotli: base.brotli,
-			newBrotli: current.brotli,
-			brotliDiff: current.brotli - base.brotli,
-
-			oldFiles: base.files,
-			newFiles: current.files,
-		};
+		return compareUpdated(base, current);
 	} else if (current) {
-		return {
-			status: "added",
-			id: current.id,
-			name: current.bundle,
-			oldSize: 0,
-			newSize: current.size,
-			sizeDiff: current.size,
-
-			oldGzip: 0,
-			newGzip: current.gzip,
-			gzipDiff: current.gzip,
-
-			oldBrotli: 0,
-			newBrotli: current.brotli,
-			brotliDiff: current.brotli,
-
-			oldFiles: [],
-			newFiles: current.files,
-		};
+		return compareAdded(current);
 	} else {
-		return {
-			status: "removed",
-			id: base.id,
-			name: base.bundle,
-			oldSize: base.size,
-			newSize: 0,
-			sizeDiff: -base.size,
-
-			oldGzip: base.gzip,
-			newGzip: 0,
-			gzipDiff: -base.gzip,
-
-			oldBrotli: base.brotli,
-			newBrotli: 0,
-			brotliDiff: -base.brotli,
-
-			oldFiles: base.files,
-			newFiles: [],
-		};
+		return compareRemoved(base);
 	}
 }
