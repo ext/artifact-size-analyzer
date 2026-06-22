322f49f93a1632c4310ccad7df81e24f0a2e5ac2
David Sveningsson <ext@sidvind.com>
2026-06-22 08:41:46 +0200

    chore(deps): update @html-validate/eslint monorepo
    Signed-off-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- analyze/lib/get-input.ts
+++ analyze/lib/get-input.ts
@@ -2,6 +2,5 @@
  * Get input from GitHub Actions
  */
 export function getInput(name: string): string {
-	/* eslint-disable-next-line unicorn/no-unsafe-property-key -- technical debt */
 	return process.env[`INPUT_${name.toUpperCase().replaceAll("-", "_")}`] ?? "";
 }
